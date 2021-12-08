4b50cfa6a5de679053a54e4e2bb22cc510ab6793
KaremAridi <80266460+KaremAridi@users.noreply.github.com>
2021-12-08 18:21:37 +0200

    worked ejs displaying data

--- controllers/exhibitionController.js
+++ controllers/exhibitionController.js
@@ -1,8 +1,8 @@
 const Exhibition = require("../models/Exhibition");
 
 const index = async (req, res) => {
-   const r =  await Exhibition.count({});
-   res.render("../views/exhibitions.ejs");
+   const r =  await Exhibition.find({});
+   res.render("../views/exhibitions.ejs",{data: r});
 };
 
 const main = (res,req) => {
