435aa3a1d181d9fc06acd52d9e105958bd2d0809
Sharif Fahes <shariffahes@hotmail.com>
2021-12-09 22:30:25 +0200

    booking page

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const ExhibitionController = require('../controllers/exhibitionController');
+const path = require("path");
 
 router.get("/", ExhibitionController.main);
 
+router.get("/book", (req,res) => {
+    res.sendFile(path.resolve(__dirname+"/../public/html/book.html"))
+});
 module.exports = router;
