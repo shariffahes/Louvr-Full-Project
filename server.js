eb95fbfa9360dc1b117be89f534849a6d5241d62
Sharif Fahes <shariffahes@hotmail.com>
2021-12-09 14:07:51 +0200

    connect 404 to server

--- server.js
+++ server.js
@@ -31,6 +31,11 @@ app.use(express.static(__dirname + "/public/html"));
 app.use(express.static(__dirname + "/public/styles"));
 app.use(express.static(__dirname + "/public/fonts"));
 app.use(express.static(__dirname + "/public/images"));
+
+app.use((_,res) => {
+    res.status(404).sendFile(__dirname+"/public/html/404.html");
+});
+
 app.listen(8081, () => {
     console.log("listening to server");
 });
